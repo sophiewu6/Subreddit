e9d278db9f7f8f487410a07ef8b52d98748bb16d
Jimbyo <jimz@uci.edu>
2019-03-10 15:24:31 -0700

    Update write_thread.java

--- app/src/main/java/com/example/reddit/write_thread.java
+++ app/src/main/java/com/example/reddit/write_thread.java
@@ -7,13 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class write_thread extends AppCompatActivity {
-    private ImageButton imageBtn; private static final int GALLERY_REQUEST_CODE = 2;
-    private Uri uri = null; private EditText textTitle; private EditText textDesc;
-    private Button postBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

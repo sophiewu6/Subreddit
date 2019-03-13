3e63ec889f876e74977f5d62cec22f07e1d39d60
sophiewu6 <wushiqi1998@gmail.com>
2019-03-12 18:49:05 -0700

    connect write thread activity

--- app/src/main/java/com/example/reddit/Subreddit.java
+++ app/src/main/java/com/example/reddit/Subreddit.java
@@ -1,9 +1,12 @@
 package com.example.reddit;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -22,6 +25,7 @@ public class Subreddit extends AppCompatActivity {
     ArrayList<Post> mThreads;
     TextView get_title;
     TextView get_desc;
+    FloatingActionButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,8 @@ public class Subreddit extends AppCompatActivity {
         get_desc = (TextView) findViewById(R.id.textView4);
         get_title.setText(MainActivity.store.getSubreddit().getTitle());
         get_desc.setText(MainActivity.store.getSubreddit().getDescription());
+
+        button = (FloatingActionButton) findViewById(R.id.floatingActionButton);
 
         mThreads = new ArrayList<Post>();
         mThreads.add(new Post("title1", "",0, 0, 0));
@@ -46,5 +52,13 @@ public class Subreddit extends AppCompatActivity {
         recyclerView.setLayoutManager(rvLayoutManager);
         mAdapter = new ThreadAdapter(mThreads);
         recyclerView.setAdapter((mAdapter));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Subreddit.this, WriteThread.class);
+                startActivity(intent);
+            }
+        });
     }
 }
